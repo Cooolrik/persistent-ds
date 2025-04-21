e245b7913302da9c1cc3f8dd5ee173e87a82a71e
Ulrik Lindahl <ulrik@ulriklindahl.com>
2025-04-21 13:18:53 +0200

    intermediate

---
+++ Include/pds/mf/Varying_MF.h
@@ -0,0 +1,165 @@
+// pds - Persistent data structure framework, Copyright (c) 2022 Ulrik Lindahl
+// Licensed under the MIT license https://github.com/Cooolrik/pds/blob/main/LICENSE
+
+#pragma once
+
+#include "../Varying.h"
+
+namespace pds
+{
+
+class Varying::MF
+{
+public:
+	static status Clear( Varying &obj );
+	static status DeepCopy( Varying &dest, const Varying *source );
+	static bool Equals( const Varying *lvar, const Varying *rvar );
+
+	static status Write( const Varying &obj, EntityWriter &writer );
+	static status Read( Varying &obj, EntityReader &reader );
+
+	static status Validate( const Varying &obj, EntityValidator &validator );
+
+	// Method to set the type of the data in the varying object, either using a parameter, or as a template method
+	static status SetType( Varying &obj, data_type_index dataType, container_type_index containerType );
+	template <class _Ty> static status SetType( Varying &obj )
+	{
+		return SetType( obj, combined_type_information<_Ty>::type_index, combined_type_information<_Ty>::container_index );
+	}
+};
+
+#include "_pds_macros.inl"
+
+status Varying::MF::Clear( Varying &obj )
+{
+	if( obj.data_m == nullptr )
+		return status::ok;
+
+	bool success = dynamic_types::clear( obj.type_m, obj.container_type_m, obj.data_m );
+	ctValidate( success, status::corrupted ) << "Could not clear the dynamic type" << ctValidateEnd;
+
+	return status::ok;
+}
+
+status Varying::MF::DeepCopy( Varying &dest, const Varying *source )
+{
+	bool success = {};
+
+	// clear the current type of dest
+	success = dest.Deinitialize();
+	ctValidate( success, status::corrupted ) << "Could not clear Varying type" << ctValidateEnd;
+
+	// if source is nullptr or empty we are done
+	if( !source || !source->IsInitialized() )
+		return status::ok;
+
+	// set type and clear any currently allocated data
+	success = SetType( dest, source->type_m, source->container_type_m );
+	ctValidate( success, status::corrupted ) << "Cannot set Varying type." << ctValidateEnd;
+
+	// copy the data from source to dest
+	success = dynamic_types::copy( dest.type_m, dest.container_type_m, dest.data_m, source->data_m );
+	ctValidate( success, status::corrupted ) << "Cannot copy Varying type data." << ctValidateEnd;
+
+	return status::ok;
+}
+
+bool Varying::MF::Equals( const Varying *lvar, const Varying *rvar )
+{
+	// early out if the pointers are equal (includes nullptr)
+	if( lvar == rvar )
+		return true;
+
+	// early out if one of the pointers is nullptr (both can't be null because of above test)
+	if( !lvar || !rvar )
+		return false;
+
+	// both pointers are valid and different pointers, compare type data
+	if( lvar->type_m != rvar->type_m )
+		return false;
+	if( lvar->container_type_m != rvar->container_type_m )
+		return false;
+
+	// types match, compare data in type
+	return dynamic_types::equals( lvar->type_m, lvar->container_type_m, lvar->data_m, rvar->data_m );
+}
+
+status Varying::MF::Write( const Varying &obj, EntityWriter &writer )
+{
+	ctValidate( obj.IsInitialized(), status::not_initialized ) << "Cannot write uninitialized Varying to stream. Use optional_value template for optional Varying data." << ctValidateEnd;
+
+	// store the data type index
+	if( !writer.Write( pdsKeyMacro( Type ), (u16)obj.type_m ) )
+		return status::cant_write;
+
+	// store the container type index
+	if( !writer.Write( pdsKeyMacro( ContainerType ), (u16)obj.container_type_m ) )
+		return status::cant_write;
+
+	// store the data
+	if( !dynamic_types::write( obj.type_m, obj.container_type_m, pdsKeyMacro( Data ), writer, obj.data_m ) )
+		return status::cant_write;
+
+	return status::ok;
+}
+
+status Varying::MF::Read( Varying &obj, EntityReader &reader )
+{
+	// if initialized, deinitalize
+	ctValidate( obj.Deinitialize(), status::corrupted ) << "Cannot Deinitialize varying data object, it is corrupted." << ctValidateEnd;
+
+	u16 type_u16 = {};
+	u16 container_type_u16 = {};
+
+	// load the data type index
+	if( !reader.Read( pdsKeyMacro( Type ), type_u16 ) )
+		return status::cant_read;
+
+	// load the container type index
+	if( !reader.Read( pdsKeyMacro( ContainerType ), container_type_u16 ) )
+		return status::cant_read;
+
+	// set the data type
+	if( !SetType( obj, (data_type_index)type_u16, (container_type_index)container_type_u16 ) )
+		return status::cant_read;
+
+	// store the data
+	if( !dynamic_types::read( obj.type_m, obj.container_type_m, pdsKeyMacro( Data ), reader, obj.data_m ) )
+		return status::cant_read;
+
+	return status::ok;
+}
+
+status Varying::MF::Validate( const Varying &obj, EntityValidator &validator )
+{
+	if( !obj.IsInitialized() )
+	{
+		pdsValidationError( ValidationError::NullNotAllowed )
+			<< "Object is not initialized, and does not have a type set. All Varying objects need to be initialized to be valid. To have an optional Varying object, use the optional_value template."
+			<< pdsValidationErrorEnd;
+	}
+
+	return status::ok;
+}
+
+status Varying::MF::SetType( Varying &obj, data_type_index dataType, container_type_index containerType )
+{
+	bool success = {};
+
+	// clear current type if it is set
+	ctValidate( obj.Deinitialize(), status::corrupted ) << "Cannot Deinitialize varying data object, it is corrupted." << ctValidateEnd;
+
+	// set type and allocate the data
+	obj.type_m = dataType;
+	obj.container_type_m = containerType;
+	std::tie( obj.data_m, success ) = dynamic_types::new_type( obj.type_m, obj.container_type_m );
+	ctValidate( success, status::not_initialized ) << "Could not set type of varying object" << ctValidateEnd;
+
+	return status::ok;
+}
+
+
+#include "_pds_undef_macros.inl"
+
+}
+// namespace pds
---
+++ Tests/VaryingTests.cpp
@@ -0,0 +1,246 @@
+// pds - Persistent data structure framework, Copyright (c) 2022 Ulrik Lindahl
+// Licensed under the MIT license https://github.com/Cooolrik/pds/blob/main/LICENSE
+
+#include "Tests.h"
+
+#include <pds/EntityValidator.h>
+#include <pds/EntityWriter.h>
+#include <pds/EntityReader.h>
+#include <pds/WriteStream.h>
+#include <pds/ReadStream.h>
+
+#include <pds/mf/Varying_MF.h>
+
+using pds::Varying;
+//
+//template<class _Kty> void ItemTableBasicTests_Validation()
+//{
+//	// check with no validation
+//	if( true )
+//	{
+//		typedef ItemTable<_Kty, TestEntityA, ItemTableFlags::NullEntities | ItemTableFlags::ZeroKeys> Dict;
+//		Dict dict;
+//
+//		EntityValidator validator;
+//		const _Kty zero = data_type_information<_Kty>::zero;
+//
+//		// add a zero key  with a null value entry
+//		dict.Entries()[zero] = std::unique_ptr<TestEntityA>();
+//		EXPECT_TRUE( dict.Entries().size() == 1 );
+//
+//		// check validation, this should succeed
+//		EXPECT_TRUE( Dict::MF::Validate( dict, validator ) );
+//		EXPECT_EQ( validator.GetErrorCount(), uint( 0 ) );
+//	}
+//
+//	// check zero key validation
+//	if( true )
+//	{
+//		typedef ItemTable<_Kty, TestEntityA, ~ItemTableFlags::ZeroKeys> Dict;
+//		Dict dict;
+//
+//		EntityValidator validator;
+//		const _Kty zero = data_type_information<_Kty>::zero;
+//
+//		// add a zero key entry (invalid)
+//		dict.Entries()[zero] = std::make_unique<TestEntityA>();
+//		EXPECT_TRUE( dict.Entries().size() == 1 );
+//
+//		// check validation, this should fail
+//		EXPECT_TRUE( Dict::MF::Validate( dict, validator ) );
+//		EXPECT_EQ( validator.GetErrorCount(), uint( 1 ) );
+//	}
+//
+//	// check null value validation
+//	if( true )
+//	{
+//		typedef ItemTable<_Kty, TestEntityA, ~ItemTableFlags::NullEntities> Dict;
+//		Dict dict;
+//
+//		EntityValidator validator;
+//		const _Kty inf_val = data_type_information<_Kty>::inf;
+//
+//		// add a null ptr value entry (invalid)
+//		dict.Entries()[inf_val] = std::unique_ptr<TestEntityA>();
+//		EXPECT_TRUE( dict.Entries().size() == 1 );
+//
+//		// check validation, this should fail
+//		EXPECT_TRUE( Dict::MF::Validate( dict, validator ) );
+//		EXPECT_EQ( validator.GetErrorCount(), uint( 1 ) );
+//	}
+//
+//	// check all validations
+//	if( true )
+//	{
+//		typedef ItemTable < _Kty, TestEntityA, ~( ItemTableFlags::NullEntities | ItemTableFlags::ZeroKeys ) > Dict;
+//		Dict dict;
+//
+//		EntityValidator validator;
+//
+//		// add a number of values, but skip zero values
+//		size_t cnt = capped_rand( 100, 300 );
+//		for( size_t i = 0; i < cnt; ++i )
+//		{
+//			const _Kty rand_val = random_value<_Kty>();
+//			if( rand_val != data_type_information<_Kty>::zero )
+//			{
+//				dict.Entries()[rand_val] = std::make_unique<TestEntityA>();
+//			}
+//		}
+//
+//		// check validation, this should fail
+//		EXPECT_TRUE( Dict::MF::Validate( dict, validator ) );
+//		EXPECT_EQ( validator.GetErrorCount(), uint( 0 ) );
+//	}
+//
+//	// test copying and moving contents of one dictionary to another 
+//	if( true )
+//	{
+//		typedef ItemTable<_Kty, TestEntityA> Dict;
+//		Dict dict;
+//
+//		// add a number of random values, record the pointers
+//		size_t cnt = capped_rand( 3, 10 );
+//		std::set<TestEntityA *> ptrs;
+//		for( size_t i = 0; i < cnt; ++i )
+//		{
+//			const _Kty rand_val = random_value<_Kty>();
+//			dict.Entries()[rand_val] = std::make_unique<TestEntityA>();
+//			dict.Entries()[rand_val]->Name() = random_value<string>();
+//			ptrs.insert( dict.Entries()[rand_val].get() );
+//		}
+//		size_t dict_size = dict.Size();
+//
+//		// make a deep copy
+//		Dict dict_copy( dict );
+//		EXPECT_TRUE( dict.Size() == dict_size );
+//		EXPECT_TRUE( dict_copy.Size() == dict_size );
+//		EXPECT_TRUE( dict_copy == dict );
+//
+//		// make sure the pointers are different (so the copy was an actual copy, and not a move)
+//		for( const auto &p : dict_copy.Entries() )
+//		{
+//			EXPECT_TRUE( ptrs.find( p.second.get() ) == ptrs.end() );
+//		}
+//
+//		// move dictionary to another
+//		Dict dict_move = std::move( dict );
+//		EXPECT_TRUE( dict.Size() == 0 );
+//		EXPECT_TRUE( dict_move.Size() == dict_size );
+//
+//		// make sure the pointers are still the same (so the move was an actual move, and not a copy)
+//		for( const auto &p : dict_move.Entries() )
+//		{
+//			EXPECT_TRUE( ptrs.find( p.second.get() ) != ptrs.end() );
+//		}
+//	}
+//
+//}
+//
+//TEST( ItemTableTests, BasicTests )
+//{
+//	setup_random_seed();
+//
+//	ItemTableBasicTests_Validation<i8>();
+//	ItemTableBasicTests_Validation<i16>();
+//	ItemTableBasicTests_Validation<i32>();
+//	ItemTableBasicTests_Validation<i64>();
+//
+//	ItemTableBasicTests_Validation<u8>();
+//	ItemTableBasicTests_Validation<u16>();
+//	ItemTableBasicTests_Validation<u32>();
+//	ItemTableBasicTests_Validation<u64>();
+//
+//	ItemTableBasicTests_Validation<float>();
+//	ItemTableBasicTests_Validation<double>();
+//
+//	ItemTableBasicTests_Validation<uuid>();
+//	ItemTableBasicTests_Validation<item_ref>();
+//	ItemTableBasicTests_Validation<hash>();
+//	ItemTableBasicTests_Validation<entity_ref>();
+//	ItemTableBasicTests_Validation<string>();
+//}
+//
+//template<class T> void ItemTableReadWriteTests_TestKeyType( const MemoryWriteStream &ws, EntityWriter &ew )
+//{
+//	typedef ItemTable<T, TestEntityA> Dict;
+//
+//	Dict random_dict;
+//
+//	// create random dictionary with random entries (half of them null)
+//	GenerateRandomItemTable<Dict>( random_dict );
+//
+//	EntityValidator validator;
+//
+//	// validate
+//	EXPECT_TRUE( Dict::MF::Validate( random_dict, validator ) );
+//
+//	// write dictionary to stream
+//	u64 start_pos = ws.GetPosition();
+//	EXPECT_TRUE( Dict::MF::Write( random_dict, ew ) );
+//
+//	// set up a temporary entity reader 
+//	MemoryReadStream rs( ws.GetData(), ws.GetSize(), ws.GetFlipByteOrder() );
+//	EntityReader er( rs );
+//	rs.SetPosition( start_pos );
+//
+//	// read back the dictionary 
+//	Dict readback_dict;
+//	EXPECT_TRUE( Dict::MF::Read( readback_dict, er ) );
+//
+//	// validate
+//	EXPECT_TRUE( Dict::MF::Validate( readback_dict, validator ) );
+//	EXPECT_EQ( validator.GetErrorCount(), uint( 0 ) );
+//
+//	// compare the values in the registries
+//	EXPECT_TRUE( random_dict.Entries().size() == readback_dict.Entries().size() );
+//	typename Dict::iterator it1 = random_dict.Entries().begin();
+//	while( it1 != random_dict.Entries().end() )
+//	{
+//		typename Dict::iterator it2 = readback_dict.Entries().find( it1->first );
+//		EXPECT_TRUE( it2 != readback_dict.Entries().end() );
+//
+//		bool has_1 = it1->second != nullptr;
+//		bool has_2 = it2->second != nullptr;
+//
+//		EXPECT_TRUE( has_1 == has_2 );
+//		if( has_1 )
+//		{
+//			EXPECT_TRUE( it1->second->Name() == it2->second->Name() );
+//		}
+//
+//		++it1;
+//	}
+//}
+//
+//TEST( ItemTableTests, ReadWriteTests )
+//{
+//	setup_random_seed();
+//
+//	for( uint pass_index = 0; pass_index < ( 2 * global_number_of_passes ); ++pass_index )
+//	{
+//		MemoryWriteStream ws;
+//		EntityWriter ew( ws );
+//
+//		ws.SetFlipByteOrder( ( pass_index & 0x1 ) != 0 );
+//
+//		ItemTableReadWriteTests_TestKeyType<i8>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<i16>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<i32>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<i64>( ws, ew );
+//
+//		ItemTableReadWriteTests_TestKeyType<u8>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<u16>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<u32>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<u64>( ws, ew );
+//
+//		ItemTableReadWriteTests_TestKeyType<float>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<double>( ws, ew );
+//
+//		ItemTableReadWriteTests_TestKeyType<uuid>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<item_ref>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<hash>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<entity_ref>( ws, ew );
+//		ItemTableReadWriteTests_TestKeyType<string>( ws, ew );
+//	}
+//}
